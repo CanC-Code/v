66b7382a330e69281f06defec1f254f65d6bd8f1
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-06-30 20:12:49 -0600

    Create app/src/main/java/com/motionforge/app/MainActivity.kt

---
+++ app/src/main/java/com/motionforge/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.motionforge.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Just a placeholder to ensure the app launches
+    }
+}
